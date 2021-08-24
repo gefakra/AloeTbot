595afd271386f0e2d712505bda53d76e7aac06ae
gefakra <61078526+gefakra@users.noreply.github.com>
2021-08-24 09:53:45 +0300

    АЛОЕ Телеграм бот
    Первая загрузка всего проекта (не пилот)

---
+++ Aloe_Telegram_FatTest/GeoPosition.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+namespace Aloe_Telegram_FatTest
+{
+   public class GeoPosition
+    {
+        public bool IsVariants { get; set; } = false;
+        public int Status { get; set; }
+        public string Region_Name { get; set; }
+        public string Sity { get; set; }
+        public string Street { get; set; }
+        public int Home { get; set; }
+        public long? Longitude { get; set; }
+        public long? Latitude { get; set; }
+
+        public void AddRegion(string msg)
+        { 
+            switch(msg)
+            {
+                case "Моё место положение":
+                   
+                    break;
+                case "Санкт-Петербург":
+                    Status = 1;
+                    Region_Name = "Санкт-Петербург";
+                    Sity = "Санкт-Петербург";
+                    IsVariants = false;
+                    break;
+                default:                    
+                    IsVariants = false;
+                    Status = 3;
+                    var t = msg.Split(new char[] {';'}).Select(x=> long.Parse(x)).ToArray();
+                    Latitude = t[0];
+                    Longitude = t[1];
+                    break;
+
+            }
+        
+        }
+
+        public void Сlear()
+        {           
+            IsVariants = false;
+            Status = 0;
+            Home = 0;
+            Sity = string.Empty;
+            Region_Name = string.Empty;
+            Street = string.Empty;
+        }
+        
+
+
+
+    }
+}
---
+++ Aloe_Telegram_FatTest/HttpHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Aloe_Telegram_FatTest
+{
+    public class HttpHelper
+    {
+            static async Task Main(string[] args)
+            {
+                using var client = new HttpClient();
+
+                var result = await client.GetAsync("http://webcode.me");
+                Console.WriteLine(result.StatusCode);
+            }
+        
+    }
+}
+
